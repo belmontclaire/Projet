b03f2caba39d54bbbbb980e901073900a0e479c2
belmontclaire <113684212+belmontclaire@users.noreply.github.com>
2022-10-21 11:07:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ placeholder = st.empty()
 
 # dataframe filter
 
-API_url = "http://127.0.0.1:5000/api/" + str(client_filter)
+API_url = "http://10.0.2.2:8080/api/" + str(client_filter)
 
 json_url = urlopen(API_url)
 
